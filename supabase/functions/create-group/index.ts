2c256c7b9e14f26bf6de5cbd32e533738a601e9e
olekgolus11 <olekgolus02@gmail.com>
2024-11-04 21:28:38 +0100

    Fix indent

--- supabase/functions/create-group/index.ts
+++ supabase/functions/create-group/index.ts
@@ -5,64 +5,64 @@ import { SupabaseService } from "../_shared/SupabaseService.ts";
 console.log("[EDGE] create-group");
 
 Deno.serve(async (req) => {
-  const token = req.headers.get("Authorization")?.replace("Bearer ", "");
-  const { name, currency } = await req.json();
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    const { name, currency } = await req.json();
 
-  if (!token) {
-    return new Response(
-      JSON.stringify({ message: "Missing token" }),
-      { status: STATUS_CODE.Unauthorized },
+    if (!token) {
+        return new Response(
+            JSON.stringify({ message: "Missing token" }),
+            { status: STATUS_CODE.Unauthorized },
+        );
+    }
+
+    const supabaseService = new SupabaseService(token);
+
+    const { data: userData, error: userError } = await supabaseService.getUser(
+        token,
     );
-  }
 
-  const supabaseService = new SupabaseService(token);
+    if (userError) {
+        return new Response(
+            JSON.stringify({ message: userError.message }),
+            { status: STATUS_CODE.Unauthorized },
+        );
+    }
 
-  const { data: userData, error: userError } = await supabaseService.getUser(
-    token,
-  );
+    const { data: group, error: groupError } = await supabaseService.supabase
+        .from("groups")
+        .insert({ name, currency, created_at: new Date() }).select();
 
-  if (userError) {
+    if (groupError) {
+        return new Response(
+            JSON.stringify({ message: groupError.message }),
+            { status: STATUS_CODE.InternalServerError },
+        );
+    }
+
+    if (!group) {
+        return new Response(
+            JSON.stringify({ message: "Group not created" }),
+            { status: STATUS_CODE.InternalServerError },
+        );
+    }
+
+    const { error: groupsProfilesError } = await supabaseService.supabase.from(
+        "groups_profiles",
+    )
+        .insert({ user_id: userData.user.id, group_id: group[0].id });
+
+    if (groupsProfilesError) {
+        return new Response(
+            JSON.stringify({ message: groupsProfilesError.message }),
+            { status: STATUS_CODE.InternalServerError },
+        );
+    }
+
     return new Response(
-      JSON.stringify({ message: userError.message }),
-      { status: STATUS_CODE.Unauthorized },
+        JSON.stringify({ group }),
+        {
+            headers: { "Content-Type": "application/json" },
+            status: STATUS_CODE.Created,
+        },
     );
-  }
-
-  const { data: group, error: groupError } = await supabaseService.supabase
-    .from("groups")
-    .insert({ name, currency, created_at: new Date() }).select();
-
-  if (groupError) {
-    return new Response(
-      JSON.stringify({ message: groupError.message }),
-      { status: STATUS_CODE.InternalServerError },
-    );
-  }
-
-  if (!group) {
-    return new Response(
-      JSON.stringify({ message: "Group not created" }),
-      { status: STATUS_CODE.InternalServerError },
-    );
-  }
-
-  const { error: groupsProfilesError } = await supabaseService.supabase.from(
-    "groups_profiles",
-  )
-    .insert({ user_id: userData.user.id, group_id: group[0].id });
-
-  if (groupsProfilesError) {
-    return new Response(
-      JSON.stringify({ message: groupsProfilesError.message }),
-      { status: STATUS_CODE.InternalServerError },
-    );
-  }
-
-  return new Response(
-    JSON.stringify({ group }),
-    {
-      headers: { "Content-Type": "application/json" },
-      status: STATUS_CODE.Created,
-    },
-  );
 });
--- supabase/functions/user-groups/index.ts
+++ supabase/functions/user-groups/index.ts
@@ -6,98 +6,98 @@ import type { Expense, Group, Payment } from "../_shared/types.ts";
 console.log("[EDGE] user-groups");
 
 Deno.serve(async (req) => {
-  const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
 
-  if (!token) {
-    return new Response(
-      JSON.stringify({ message: "Missing token" }),
-      { status: STATUS_CODE.Unauthorized },
-    );
-  }
-
-  const supabaseService = new SupabaseService(token);
-
-  const { data, error } = await supabaseService.getUser(token);
-
-  if (error) {
-    return new Response(
-      JSON.stringify({ message: error.message }),
-      { status: STATUS_CODE.Unauthorized },
-    );
-  }
-
-  const groups = await supabaseService.supabase
-    .from("groups")
-    .select(
-      `id, name, currency, created_at, groups_profiles ( user_id, group_id )`,
-    )
-    .eq("groups_profiles.user_id", data.user.id);
-
-  if (groups.error) {
-    return new Response(
-      JSON.stringify({ message: groups.error.message }),
-      { status: STATUS_CODE.InternalServerError },
-    );
-  }
-
-  const groupsIds = groups.data.map((group: Group) => group.id);
-
-  const userPayments = await Promise.allSettled(
-    groupsIds.map(async (groupId: string) => {
-      return await supabaseService.supabase
-        .from("expenses")
-        .select(
-          `description, payments ( expense_id, user_id, amount, state)`,
-        )
-        .eq("group_id", groupId)
-        .eq("payments.user_id", data.user.id);
-    }),
-  );
-
-  const groupSums = groupsIds.reduce(
-    (acc: Record<string, number>, groupId: string, index: number) => {
-      const result = userPayments[index];
-
-      if (
-        result.status === "fulfilled" && !result.value.error
-      ) {
-        const sum = result.value.data.reduce(
-          (total: number, expense: Expense) => {
-            const payments = expense.payments.filter((payment) =>
-              payment.state === "pending"
-            ) || [];
-            return total +
-              payments.reduce(
-                (paymentTotal: number, payment: Payment) =>
-                  paymentTotal + (payment.amount || 0),
-                0,
-              );
-          },
-          0,
+    if (!token) {
+        return new Response(
+            JSON.stringify({ message: "Missing token" }),
+            { status: STATUS_CODE.Unauthorized },
         );
+    }
 
-        acc[groupId] = sum;
-      } else {
-        acc[groupId] = 0;
-      }
+    const supabaseService = new SupabaseService(token);
 
-      return acc;
-    },
-    {},
-  );
+    const { data, error } = await supabaseService.getUser(token);
 
-  const groupsWithSums = groups.data.map((group: Group) => ({
-    ...group,
-    totalAmount: groupSums[group.id] || 0,
-  }));
+    if (error) {
+        return new Response(
+            JSON.stringify({ message: error.message }),
+            { status: STATUS_CODE.Unauthorized },
+        );
+    }
 
-  return new Response(
-    JSON.stringify({
-      groups: groupsWithSums,
-    }),
-    {
-      headers: { "Content-Type": "application/json" },
-      status: STATUS_CODE.OK,
-    },
-  );
+    const groups = await supabaseService.supabase
+        .from("groups")
+        .select(
+            `id, name, currency, created_at, groups_profiles ( user_id, group_id )`,
+        )
+        .eq("groups_profiles.user_id", data.user.id);
+
+    if (groups.error) {
+        return new Response(
+            JSON.stringify({ message: groups.error.message }),
+            { status: STATUS_CODE.InternalServerError },
+        );
+    }
+
+    const groupsIds = groups.data.map((group: Group) => group.id);
+
+    const userPayments = await Promise.allSettled(
+        groupsIds.map(async (groupId: string) => {
+            return await supabaseService.supabase
+                .from("expenses")
+                .select(
+                    `description, payments ( expense_id, user_id, amount, state)`,
+                )
+                .eq("group_id", groupId)
+                .eq("payments.user_id", data.user.id);
+        }),
+    );
+
+    const groupSums = groupsIds.reduce(
+        (acc: Record<string, number>, groupId: string, index: number) => {
+            const result = userPayments[index];
+
+            if (
+                result.status === "fulfilled" && !result.value.error
+            ) {
+                const sum = result.value.data.reduce(
+                    (total: number, expense: Expense) => {
+                        const payments = expense.payments.filter((payment) =>
+                            payment.state === "pending"
+                        ) || [];
+                        return total +
+                            payments.reduce(
+                                (paymentTotal: number, payment: Payment) =>
+                                    paymentTotal + (payment.amount || 0),
+                                0,
+                            );
+                    },
+                    0,
+                );
+
+                acc[groupId] = sum;
+            } else {
+                acc[groupId] = 0;
+            }
+
+            return acc;
+        },
+        {},
+    );
+
+    const groupsWithSums = groups.data.map((group: Group) => ({
+        ...group,
+        totalAmount: groupSums[group.id] || 0,
+    }));
+
+    return new Response(
+        JSON.stringify({
+            groups: groupsWithSums,
+        }),
+        {
+            headers: { "Content-Type": "application/json" },
+            status: STATUS_CODE.OK,
+        },
+    );
 });
